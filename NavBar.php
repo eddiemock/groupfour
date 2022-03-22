0dfdee7f518240503b361f4876473cf3ee32b1e4
eddiemock <87999582+eddiemock@users.noreply.github.com>
2022-03-22 10:21:14 +0000

    Create NavBar.php

---
+++ NavBar.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Bank of Ed</title>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/myproject/site.css" />
+	
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
+  
+  </head>
+
+<body class="bgColor">
+	<header>
+			<nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+				<div class="container">
+					<a class="navbar-brand" href="/myproject/Index.php">Bank of Ed</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarSupportedContent"
+							aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
+						<ul class="navbar-nav flex-grow-1">
+							<li class="nav-item">
+								<a class="nav-link text-dark" href="/myproject/">
+									<img border="0" alt="User Icon" src="/myproject/Icons/Jedi.png" width="25" height="25">
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link text-dark" href="/myproject/createUser.php">Register for Interest</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link text-dark" href="/myproject/userLogin.php">User Login</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link text-dark" href="/myproject/Admin/LoginAdmin.php">Admin Login</a>
+							</li>
+
+						</ul>
+					</div>
+				</div>
+			</nav>
+	</header>
