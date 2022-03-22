97e8a113b39083c3057822b7f6bb0e063ddafd36
eddiemock <87999582+eddiemock@users.noreply.github.com>
2022-03-22 10:21:51 +0000

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,23 @@
+<?php require("NavBar.php");?>
+
+	<div class="container bgColor">
+        	<main role="main" class="pb-3">
+
+				<br><h2>Welcome to the landing page</h2><br>
+
+				This is a website written in PHP, that I have been working on for Databases & The Web.
+
+				<br><br><br>
+				<a href="/myproject/createUser.php">Create User</a>
+				- This allows you to insert new users into the database.
+
+				<br><br>
+				<a href="/myproject/viewUser.php">View User</a>
+				- This allows you to view a list of users that are currently in the database.
+
+			</main>
+	</div>
+
+<?php require("Footer.php");?>
+
+
